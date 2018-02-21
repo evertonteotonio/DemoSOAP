bc5d5de12c5ce8b80dcbdf7b894b205395bdd1e9
logonrm <logonrm@fiap.com.br>
2018-02-21 20:46:41 -0300

    first commit

---
+++ app/src/main/java/com/example/logonrm/demosoap/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.logonrm.demosoap
+
+import android.os.AsyncTask
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private val url = "http://pa02micro42:8080/CalculadoraWSService/CalculadoraWS?wsdl"
+    private  val nameSpace = "http://heiderlopes.com.br/"
+    private  val methodName = "calcular"
+    private  val soapAction = nameSpace + methodName
+    private  val parametro1 = "num1"
+    private  val parametro2 = "num2"
+    private  val parametro3 = "op"
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    inner class CallWebService: AsyncTask<String, Void, String>() {
+
+        override fun doInBackground(vararg params: String?): String {
+
+        }
+
+    }
+
+}
